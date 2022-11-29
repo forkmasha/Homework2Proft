874393581dafbe9e1aa27f278ebdd319ecf8c02c
Asus <forkmasha100@gmail.com>
2022-11-29 15:33:04 +0200

    Changes

--- src/main/java/task2/Record.java
+++ src/main/java/task2/Record.java
@@ -1,95 +1,62 @@
 package task2;
-
-
-// read xml file as objects
-// "date_time: "2020-05-05 15:39:03", //
-//"first_name": "Ivan",
-//"last_name": "Ivanov"
-//"type": "SPEEDING", //
-//"fine_amount": 340.00 // cyMa urpady
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
-// create object with parameter:
-// date_time, first_name, last_name, type, fine_amount
-
 public class Record {
-    private String date_time;
-    private String first_name;
-    private String last_name;
-    private String type;
-    private double fine_amount;
+    private String DateTime;
+    private String FirstName;
+    private String LastName;
+    private String Type;
+    private double FineAmount;
 
-    public Record(String date_time, String first_name, String last_name, String type, double fine_amount) {
-        this.date_time = date_time;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.type = type;
-        this.fine_amount = fine_amount;
+    public Record(String DateTime, String FirstName, String LastName, String type, double FineAmount) {
+        this.DateTime = DateTime;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Type = type;
+        this.FineAmount = FineAmount;
     }
 
-//	public String getDate_time() {
-//		return date_time;
-//	}
 
-    public void setDate_time(String date_time) {
-        this.date_time = date_time;
+    public void setDateTime(String dateTime) {
+        this.DateTime = dateTime;
     }
 
-//	public String getFirst_name() {
-//		return first_name;
-//	}
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
     }
 
-//	public String getLast_name() {
-//		return last_name;
-//	}
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
     }
 
     public String getType() {
-        return type;
+        return Type;
     }
 
     public void setType(String type) {
-        this.type = type;
+        this.Type = type;
     }
 
-    public double getFine_amount() {
-        return fine_amount;
+    public double getFineAmount() {
+        return FineAmount;
     }
 
-    public void setFine_amount(double fine_amount) {
-        this.fine_amount = fine_amount;
+    public void setFineAmount(double fineAmount) {
+        this.FineAmount = fineAmount;
     }
 
     @Override
     public String toString() {
-        return "Record [date_time=" + date_time + ", first_name=" + first_name + ", last_name=" + last_name + ", type="
-                + type + ", fine_amount=" + fine_amount + "]";
+        return "Record [date_time=" + DateTime + ", first_name=" + FirstName + ", last_name=" + LastName + ", type="
+                + Type + ", fine_amount=" + FineAmount + "]";
     }
 
 
-    // get all types of records
     public static List<String> getTypes(List<Record> records) {
         List<String> types = new ArrayList<>();
         for (Record record : records) {
@@ -99,8 +66,6 @@ public class Record {
         }
         return types;
     }
-
-    // divide records by types
 
     public static List<Record> getRecordsByType(List<Record> records, String type) {
         List<Record> recordsByType = new ArrayList<>();
@@ -113,79 +78,11 @@ public class Record {
     }
 
 
-    public static Comparator<Record> fine_amountComparator = (o1, o2) -> (int) (o2.getFine_amount() - o1.getFine_amount());
 
-    public static List<Record> file2list(String filename) throws ParserConfigurationException, IOException, SAXException {
-        // read xml file
-        File file = new File(filename);
+    public static Comparator<Record> fine_amountComparator = (o1, o2) -> (int) (o2.getFineAmount() - o1.getFineAmount());
 
-        // create document builder factory
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-        // create document builder
-        DocumentBuilder builder = factory.newDocumentBuilder();
 
-        // create document
-        Document document = builder.parse(file);
 
-        // get root element
-        Element root = document.getDocumentElement();
-
-        // get all child element
-        NodeList nodes = root.getChildNodes();
-
-        List<Record> list = new ArrayList<>();
-
-        // get all child element from list
-        for (int i = 0; i < nodes.getLength(); i++) {
-            Node node = nodes.item(i);
-
-            // check if node is element
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) node;
-
-                // get all child element from element
-                NodeList childNodes = element.getChildNodes();
-
-                // create object
-                Record record = new Record(null, null, null, null, 0);
-
-                //insert data
-                for (int j = 0; j < childNodes.getLength(); j++) {
-                    Node childNode = childNodes.item(j);
-
-                    // check if child node is element
-                    if (childNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element childElement = (Element) childNode;
-
-                        // get value from element
-                        String value = childElement.getTextContent();
-
-                        // insert data to object
-                        switch (childElement.getNodeName()) {
-                            case "date_time":
-                                record.setDate_time(value);
-                                break;
-                            case "first_name":
-                                record.setFirst_name(value);
-                                break;
-                            case "last_name":
-                                record.setLast_name(value);
-                                break;
-                            case "type":
-                                record.setType(value);
-                                break;
-                            case "fine_amount":
-                                record.setFine_amount(Double.parseDouble(value));
-                                break;
-                        }
-                    }
-                }
-                // add object to list
-                list.add(record);
-            }
-        }
-        return list;
-    }
 }
 
--- src/main/java/task2/Runner.java
+++ src/main/java/task2/Runner.java
@@ -1,11 +1,17 @@
 package task2;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileWriter;
@@ -17,64 +23,148 @@ public class Runner {
 
 	public static String path = "src/main/java/task2/";
 
+	public static List<Record> file2list(String filename) throws ParserConfigurationException, IOException, SAXException {
+		// read xml file
+		File file = new File(filename);
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		// create document builder factory
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		// create document builder
+		DocumentBuilder builder = factory.newDocumentBuilder();
+
+		// create document
+		Document document = builder.parse(file);
+
+		// get root element
+		Element root = document.getDocumentElement();
+
+		// get all child element
+		NodeList nodes = root.getChildNodes();
+
+		List<Record> list = new ArrayList<>();
+
+		// get all child element from list
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+
+			// check if node is element
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+
+				// get all child element from element
+				NodeList childNodes = element.getChildNodes();
+
+				// create object
+				Record record = new Record(null, null, null, null, 0);
+
+				//insert data
+				for (int j = 0; j < childNodes.getLength(); j++) {
+					Node childNode = childNodes.item(j);
+
+					// check if child node is element
+					if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element childElement = (Element) childNode;
+
+						// get value from element
+						String value = childElement.getTextContent();
+
+						// insert data to object
+						switch (childElement.getNodeName()) {
+							case "date_time" -> record.setDateTime(value);
+							case "first_name" -> record.setFirstName(value);
+							case "last_name" -> record.setLastName(value);
+							case "type" -> record.setType(value);
+							case "fine_amount" -> record.setFineAmount(Double.parseDouble(value));
+						}
+					}
+				}
+				// add object to list
+				list.add(record);
+			}
+		}
+		return list;
+	}
+
+
+	public static void main(String[] args){
 
 		// List of objects
-		List<Record> list = new ArrayList<>();
-		List<Record> newlist = new ArrayList<>();
+		List<Record> List = new ArrayList<>();
+		List<Record> NewList = new ArrayList<>();
 
-		// find all *.xml files and add to list
-		File folder = new File(path);
-		File[] listOfFiles = folder.listFiles();
+		// try file exception
+		try {
+			// find all *.xml files and add to list
+			File folder = new File(path);
+			File[] listOfFiles = folder.listFiles();
 
-		// if no files found print message
-		assert listOfFiles != null;
-		if (listOfFiles.length == 0) {
-			System.out.println("No files found");
+			// if no files found print message
+			assert listOfFiles != null;
+			if (listOfFiles.length == 0) {
+				System.out.println("No files found");
+				throw new IOException();
+				//System.exit(-1);
+			}
+
+			for (File file : listOfFiles) {
+				if (file.isFile()) {
+					if (file.getName().endsWith(".xml")) {
+						List.addAll(file2list(file.getAbsolutePath()));
+					}
+				}
+			}
+		} catch (Exception e) {
+			switch (e.getClass().getName()) {
+				case "java.io.FileNotFoundException" -> System.out.println("File not found");
+				case "java.io.IOException" -> System.out.println("IO error");
+				case "org.xml.sax.SAXParseException" -> System.out.println("XML parse error");
+				default -> System.out.println("Unknown error");
+			}
 			System.exit(-1);
 		}
 
-		for (File file : listOfFiles) {
-			if (file.isFile()) {
-				if (file.getName().endsWith(".xml")) {
-					list.addAll(Record.file2list(file.getAbsolutePath()));
-				}
-			}
-		}
-
 		// print sorted array
-		list.stream().sorted(Record.fine_amountComparator).forEach(System.out::println);
+		//List.stream().sorted(Record.fine_amountComparator).forEach(System.out::println);
 
 		// sum all fines form same type and sort form max to min then write as json file
-		List<String> types = Record.getTypes(list);
+		List<String> types = Record.getTypes(List);
 		List<Record> recordsByType;
 
 		for (String type : types) {
-			recordsByType = Record.getRecordsByType(list, type);
+			recordsByType = Record.getRecordsByType(List, type);
 			double sum = 0;
 			for (Record record : recordsByType) {
-				sum += record.getFine_amount();
+				sum += record.getFineAmount();
 			}
 			Record record = new Record(null, null, null, type, sum);
-			newlist.add(record);
+			NewList.add(record);
 		}
 
-		// write as json file using JsonWriter
-		File jsonFile = new File(path+"report.json");
-		FileWriter writer = new FileWriter(jsonFile);
-		JsonWriter jsonWriter = Json.createWriter(writer);
-		JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+		try{
+			// write as json file using JsonWriter
+			File jsonFile = new File(path+"report.json");
+			FileWriter writer = new FileWriter(jsonFile);
+			JsonWriter jsonWriter = Json.createWriter(writer);
+			JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
-		for (Record record : newlist) {
-			JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-			objectBuilder.add("type", record.getType());
-			objectBuilder.add("fine_amount", record.getFine_amount());
-			arrayBuilder.add(objectBuilder);
+			for (Record record : NewList) {
+				JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+				objectBuilder.add("type", record.getType());
+				objectBuilder.add("fine_amount", record.getFineAmount());
+				arrayBuilder.add(objectBuilder);
+			}
+
+			jsonWriter.writeArray(arrayBuilder.build());
+			jsonWriter.close();
+			writer.close();
+		} catch (Exception e) {
+			switch (e.getClass().getName()) {
+				case "java.io.FileNotFoundException" -> System.out.println("File not found");
+				case "java.io.IOException" -> System.out.println("IO error");
+				default -> System.out.println("Unknown error");
+			}
+			System.exit(-1);
 		}
-
-		jsonWriter.writeArray(arrayBuilder.build());
-		jsonWriter.close();
-		writer.close();
 	}
 }
